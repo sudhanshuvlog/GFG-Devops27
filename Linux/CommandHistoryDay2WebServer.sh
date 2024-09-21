4085939084b55079e9d7fcabc8fa9bd98da42d14
Sudhanshu Pandey <57751726+sudhanshuvlog@users.noreply.github.com>
2024-09-21 10:25:58 +0530

    Create CommandHistoryDay2WebServer.sh

---
+++ Linux/CommandHistoryDay2WebServer.sh
@@ -0,0 +1,77 @@
+history
+
+    1  whoami
+
+    2  yum install httpd
+
+    3  rpm -q httpd
+
+    4  yum install httpd
+
+    5  cd /var/www/html/
+
+    6  ls
+
+    7  cat > index.html
+
+    8  cat index.html 
+
+    9  systemctl start httpd
+
+   10  systemctl --help
+
+   11  systemctl status httpd
+
+   12  cd /
+
+   13  ls
+
+   14  cd etc
+
+   15  ls
+
+   16  cd httpd/
+
+   17  ls
+
+   18  cd conf
+
+   19  ls
+
+   20  pwd
+
+   21  cat httpd.conf 
+
+   22  clear
+
+   23  vi httpd.conf 
+
+   24  systemctl restart httpd
+
+   25  pwd
+
+   26  cd /
+
+   27  ls
+
+   28  cd var/
+
+   29  ls
+
+   30  cd log/
+
+   31  ls
+
+   32  cd httpd/
+
+   33  ls
+
+   34  cat access_log 
+
+   35  vi app.py
+
+   36  python3 app.py 
+
+   37  yum whatprovides nginx
+
+   38  history
